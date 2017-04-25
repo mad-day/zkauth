eeb2af3a983adb6601f07a4e551ecefd3582d91e
Simon Schmidt <simon.schmidt@htp-tel.de>
2017-04-25 15:03:51 +0200

    Initial Import

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # zkauth
 zero knowledge authentication based on the socialist millionaire protocol
+
+
+```go
+	x := sha3.NewShake256()
+	x.Write([]byte("1234"))
+	
+	seed := []byte("seedSEED")
+	
+	Ma := []byte("secret123")
+	Mb := []byte("secret123")
+	
+	hs1 := new(zkauth.Handshake)
+	hs2 := new(zkauth.Handshake)
+	
+	hs1.Mod = new(big.Int)
+	hs1.Mod.SetString("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFF61",16)
+	hs2.Mod = hs1.Mod
+	
+	hs1.Message = Ma
+	hs2.Message = Mb
+	hs1.Primary = true
+	
+	{
+		A,Al,_ := hs1.Step1(seed,x,4,32)
+		B,Bl,_ := hs2.Step1(seed,x,4,32)
+		fmt.Println(A)
+		fmt.Println(Al)
+		fmt.Println(B)
+		fmt.Println(Bl)
+		Pa,Qa := hs1.Step2(B,Bl)
+		Pb,Qb := hs2.Step2(A,Al)
+		fmt.Println(Pa)
+		fmt.Println(Qa)
+		fmt.Println(Pb)
+		fmt.Println(Qb)
+		QQa := hs1.Step3(Pb,Qb)
+		QQb := hs2.Step3(Pa,Qa)
+		fmt.Println(QQa)
+		fmt.Println(QQb)
+		hs1.Step4(QQb)
+		hs2.Step4(QQa)
+	}
+	
+	fmt.Println("Failed?",hs1.Failed,hs2.Failed)
+```
---
+++ csteps.go
@@ -0,0 +1,114 @@
+/*
+MIT License
+
+Copyright (c) 2017 Simon Schmidt
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/* Zero knowledge authentication based on the socialist millionaire protocol. */
+package zkauth
+
+import "golang.org/x/crypto/sha3"
+import "github.com/mad-day/hypercomplex"
+import "math/big"
+import "io"
+import "errors"
+
+var Failed = errors.New("Failed")
+
+func isOneOrZero(c hypercomplex.MultiComp) bool {
+	var O,I big.Int
+	O.SetInt64(0)
+	I.SetInt64(1)
+	if I.Cmp(c[0])<0 { return false }
+	for _,cc := range c[1:] {
+		if O.Cmp(cc)==0 { return false }
+	}
+	return true
+}
+func isEq(a, b hypercomplex.MultiComp) bool {
+	for i,c := range a {
+		if c.Cmp(b[i])!=0 { return false }
+	}
+	return true
+}
+
+type Handshake struct{
+	hypercomplex.Modulus
+	H hypercomplex.MultiComp
+	A, Al, R []byte
+	Primary bool
+	G, Y, P, Q, ShouldC hypercomplex.MultiComp
+	Message []byte
+	Failed bool
+}
+func (h *Handshake) Step1(seed []byte,r io.Reader,mcLen,pkLen int) (A,Al hypercomplex.MultiComp, err error){
+	{
+		x := sha3.NewShake256()
+		x.Write(seed)
+		h.H,_ = h.Deterministic(x,mcLen)
+	}
+	h.Failed = false
+	h.A  = make([]byte,pkLen)
+	h.Al = make([]byte,pkLen)
+	h.R  = make([]byte,pkLen)
+	
+	_,err = io.ReadFull(r,h.A)
+	if err!=nil { return }
+	_,err = io.ReadFull(r,h.Al)
+	if err!=nil { return }
+	_,err = io.ReadFull(r,h.R)
+	if err!=nil { return }
+	
+	A  = h.Exp(h.H,h.A)
+	Al = h.Exp(h.H,h.Al)
+	return
+}
+func (h* Handshake) Step2(A,Al hypercomplex.MultiComp) (P,Q hypercomplex.MultiComp){
+	if isOneOrZero(A) || isOneOrZero(Al) { h.Failed = true }
+	h.G = h.Exp(A ,h.A)
+	h.Y = h.Exp(Al,h.Al) 
+	h.P = h.Exp(h.Y ,h.R)
+	h.Q = h.Multiply( h.Exp(h.H ,h.R), h.Exp(h.G, h.Message) )
+	P = h.P
+	Q = h.Q
+	return
+}
+func (h* Handshake) Step3(P,Q hypercomplex.MultiComp) hypercomplex.MultiComp{
+	Q1 := h.Q
+	Q2 := Q
+	P1 := h.P
+	P2 := P
+	if isEq(Q1,Q2)||isEq(P1,P2) { h.Failed = true }
+	if h.Primary {
+		Q2 = h.Inverse(Q2)
+		P2 = h.Inverse(P2)
+	}else{
+		Q1 = h.Inverse(Q1)
+		P1 = h.Inverse(P1)
+	}
+	QQa := h.Exp(h.Multiply(Q1,Q2),h.Al)
+	h.ShouldC = h.Multiply(P1,P2)
+	return QQa
+}
+func (h* Handshake) Step4(QQb hypercomplex.MultiComp) {
+	c := h.Exp(QQb,h.Al)
+	if !isEq(c,h.ShouldC) { h.Failed = true }
+}
